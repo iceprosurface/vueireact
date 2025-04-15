852d8e2aa687b32186b54934f0122b69f39214fb
icepro <icepro@xd.com>
2025-04-15 13:43:40 +0800

    fix: update tsBuildInfoFile path in tsconfig and refine type inference in toVues function

--- packages/core/src/toVue.ts
+++ packages/core/src/toVue.ts
@@ -5,7 +5,7 @@ export function toVue<Props, SetupContext>(component: (props: Props, ctx: SetupC
   return makeFC(component) as any;
 }
 export function toVues<FCRecord extends Record<string, (props: any, ctx: any) => () => JSX.Element>>(components: FCRecord): {
-  [K in keyof FCRecord]: FCRecord[K] extends (props: infer P, ctx: any) => () => JSX.Element ? DefineComponent<P, any> : never
+  [K in keyof FCRecord]: FCRecord[K] extends (props: infer P, ctx: any) => () => JSX.Element ? DefineComponent<Omit<P, 'children'>, any> : never
 } {
   return {
     ...Object.fromEntries(Object.entries(components).map(([key, value]) => [key, toVue(value)])) as any,
