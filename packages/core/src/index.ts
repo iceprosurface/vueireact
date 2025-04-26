9793e89fd4176bfb17696a3665bcf9fb83243c09
icepro <icepro@xd.com>
2025-04-26 17:35:56 +0800

    fix: correct import path for withDirectives.js

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,4 +3,4 @@ export * from './defineExpose.js';
 export * from './toVue.js';
 export * from './withDefaults.js';
 export * from './components/index.js';
-export * from './withDirectives';
+export * from './withDirectives.js';
